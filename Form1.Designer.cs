9206088e0ba321ef7dca963fe5e9f8ab4c2a7fc7
gbell <gbell@GREGS-LAPTOP>
2022-09-21 19:16:50 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace SpecialProject1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnR1C1 = new System.Windows.Forms.Button();
+            this.btnR1C2 = new System.Windows.Forms.Button();
+            this.btnR1C3 = new System.Windows.Forms.Button();
+            this.btnR2C1 = new System.Windows.Forms.Button();
+            this.btnR2C2 = new System.Windows.Forms.Button();
+            this.btnR2C3 = new System.Windows.Forms.Button();
+            this.btnR3C1 = new System.Windows.Forms.Button();
+            this.btnR3C2 = new System.Windows.Forms.Button();
+            this.btnR3C3 = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnR1C1
+            // 
+            this.btnR1C1.Location = new System.Drawing.Point(175, 109);
+            this.btnR1C1.Name = "btnR1C1";
+            this.btnR1C1.Size = new System.Drawing.Size(52, 46);
+            this.btnR1C1.TabIndex = 0;
+            this.btnR1C1.UseVisualStyleBackColor = true;
+            this.btnR1C1.Click += new System.EventHandler(this.button_Click);
+            // 
+            // btnR1C2
+            // 
+            this.btnR1C2.Location = new System.Drawing.Point(275, 109);
+            this.btnR1C2.Name = "btnR1C2";
+            this.btnR1C2.Size = new System.Drawing.Size(59, 46);
+            this.btnR1C2.TabIndex = 1;
+            this.btnR1C2.UseVisualStyleBackColor = true;
+            this.btnR1C2.Click += new System.EventHandler(this.button_Click);
+            // 
+            // btnR1C3
+            // 
+            this.btnR1C3.Location = new System.Drawing.Point(378, 109);
+            this.btnR1C3.Name = "btnR1C3";
+            this.btnR1C3.Size = new System.Drawing.Size(58, 46);
+            this.btnR1C3.TabIndex = 2;
+            this.btnR1C3.UseVisualStyleBackColor = true;
+            this.btnR1C3.Click += new System.EventHandler(this.button_Click);
+            // 
+            // btnR2C1
+            // 
+            this.btnR2C1.Location = new System.Drawing.Point(175, 187);
+            this.btnR2C1.Name = "btnR2C1";
+            this.btnR2C1.Size = new System.Drawing.Size(52, 49);
+            this.btnR2C1.TabIndex = 3;
+            this.btnR2C1.UseVisualStyleBackColor = true;
+            this.btnR2C1.Click += new System.EventHandler(this.button_Click);
+            // 
+            // btnR2C2
+            // 
+            this.btnR2C2.Location = new System.Drawing.Point(275, 187);
+            this.btnR2C2.Name = "btnR2C2";
+            this.btnR2C2.Size = new System.Drawing.Size(59, 49);
+            this.btnR2C2.TabIndex = 4;
+            this.btnR2C2.UseVisualStyleBackColor = true;
+            this.btnR2C2.Click += new System.EventHandler(this.button_Click);
+            // 
+            // btnR2C3
+            // 
+            this.btnR2C3.Location = new System.Drawing.Point(378, 187);
+            this.btnR2C3.Name = "btnR2C3";
+            this.btnR2C3.Size = new System.Drawing.Size(58, 49);
+            this.btnR2C3.TabIndex = 5;
+            this.btnR2C3.UseVisualStyleBackColor = true;
+            this.btnR2C3.Click += new System.EventHandler(this.button_Click);
+            // 
+            // btnR3C1
+            // 
+            this.btnR3C1.Location = new System.Drawing.Point(175, 256);
+            this.btnR3C1.Name = "btnR3C1";
+            this.btnR3C1.Size = new System.Drawing.Size(52, 49);
+            this.btnR3C1.TabIndex = 6;
+            this.btnR3C1.UseVisualStyleBackColor = true;
+            this.btnR3C1.Click += new System.EventHandler(this.button_Click);
+            // 
+            // btnR3C2
+            // 
+            this.btnR3C2.Location = new System.Drawing.Point(278, 256);
+            this.btnR3C2.Name = "btnR3C2";
+            this.btnR3C2.Size = new System.Drawing.Size(56, 49);
+            this.btnR3C2.TabIndex = 7;
+            this.btnR3C2.UseVisualStyleBackColor = true;
+            this.btnR3C2.Click += new System.EventHandler(this.button_Click);
+            // 
+            // btnR3C3
+            // 
+            this.btnR3C3.Location = new System.Drawing.Point(378, 256);
+            this.btnR3C3.Name = "btnR3C3";
+            this.btnR3C3.Size = new System.Drawing.Size(58, 49);
+            this.btnR3C3.TabIndex = 8;
+            this.btnR3C3.UseVisualStyleBackColor = true;
+            this.btnR3C3.Click += new System.EventHandler(this.button_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(175, 365);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(90, 35);
+            this.btnExit.TabIndex = 9;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(558, 450);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnR3C3);
+            this.Controls.Add(this.btnR3C2);
+            this.Controls.Add(this.btnR3C1);
+            this.Controls.Add(this.btnR2C3);
+            this.Controls.Add(this.btnR2C2);
+            this.Controls.Add(this.btnR2C1);
+            this.Controls.Add(this.btnR1C3);
+            this.Controls.Add(this.btnR1C2);
+            this.Controls.Add(this.btnR1C1);
+            this.Name = "Form1";
+            this.Text = "TicTacToe";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button btnR1C1;
+        private Button btnR1C2;
+        private Button btnR1C3;
+        private Button btnR2C1;
+        private Button btnR2C2;
+        private Button btnR2C3;
+        private Button btnR3C1;
+        private Button btnR3C2;
+        private Button btnR3C3;
+        private Button btnExit;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,75 @@
+namespace SpecialProject1
+
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int turnCount = 0;
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            // sets the variable button to the button used to access event
+            var button = sender as Button;
+                
+            //Validates if the button is empty 
+                if (button.Text == "")
+                {
+                //If the space is empty checks what turn it is and outputs an X or O respectively
+                    if (turnCount % 2 == 0)
+                    {
+                        button.Text = "X";
+                    }
+                    else
+                    {
+                        button.Text = "O";
+                    }
+
+                    //Advances to the next turn
+                    turnCount++;
+                }
+                //If the space is already occupied an error message displays
+                else
+                {
+                    MessageBox.Show("Invalid Choice");
+                }
+
+            if (turnCount >= 5) {
+                CheckWinner();
+            }
+                
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    
+        private void CheckWinner()
+        {
+            //Method checks for a winner and displays a winner message
+
+            //Checks from right corner across the top/down left side/ diagnol from top left to bottom right
+            if (btnR1C1.Text != "" && btnR1C1.Text == btnR1C2.Text && btnR1C2.Text == btnR1C3.Text
+                || btnR1C1.Text == btnR2C1.Text && btnR2C1.Text == btnR3C1.Text || btnR1C1.Text
+                == btnR2C2.Text && btnR2C2.Text == btnR3C3.Text) { MessageBox.Show($"Winner! 3 {btnR1C1.Text}'s in a row!"); }
+            
+            //checks from bottom left corner up right side / across bottom
+            if (btnR3C3.Text != "" && btnR3C3.Text == btnR3C2.Text && btnR3C2.Text == btnR3C1.Text
+                || btnR3C3.Text == btnR2C3.Text && btnR2C3.Text == btnR1C1.Text) { MessageBox.Show($"Winner! 3 {btnR3C3.Text}'s in a row!"); }
+            
+            // checks diagnolly from bottom left to top right
+            if (btnR1C3.Text != "" && btnR1C3.Text == btnR2C2.Text && btnR3C1.Text == btnR2C2.Text) { MessageBox.Show($"Winner! 3 {btnR3C3.Text}'s in a row!"); }
+
+            //checks the middle 3 buttons across
+            if (btnR1C2.Text != "" && btnR1C2.Text == btnR2C2.Text && btnR3C2.Text == btnR2C2.Text) { MessageBox.Show($"Winner! 3 {btnR1C2.Text}'s in a row!"); }
+
+            //checks the middle 3 buttons top down
+            if (btnR2C1.Text != "" && btnR2C1.Text == btnR2C2.Text && btnR2C3.Text == btnR2C2.Text) { MessageBox.Show($"Winner! 3 {btnR2C1.Text}'s in a row!"); }
+        }
+    
+    }
+}
